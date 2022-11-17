a0e0701b3f13fa09b793c5e518a256f41a04555b
Nikitakakoito <84241612+nikitakakoito@users.noreply.github.com>
2022-11-18 00:09:18 +0300

    Update UserIDChecker.py

--- UserIDChecker.py
+++ UserIDChecker.py
@@ -10,6 +10,7 @@ async def start(message: types.message):
 @dp.message_handler(commands=['getuserid'])
 async def getuserid(message: types.Message):
     await message.answer(f'userid received, thanks!\n\nYour userId: {message.from_user.id}')
+    print(message.from_user.id)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
